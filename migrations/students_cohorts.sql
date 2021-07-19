2a2bc583ec609cba06fbf819390f4fbae4868a86
Yves Candau <ycandau@gmail.com>
2021-07-19 18:49:11 +0000

    Add query to select all students in a given cohort

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -1,3 +1,10 @@
+CREATE TABLE cohorts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  start_date DATE,
+  end_date DATE
+);
+
 CREATE TABLE students (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
@@ -7,11 +14,4 @@ CREATE TABLE students (
   start_date DATE,
   end_date DATE,
   cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
-);
-
-CREATE TABLE cohorts (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255),
-  start_date DATE,
-  end_date DATE
 );
